84627392aae083367faa3e9a480bc332da781820
Turolol <sespa1234@gmail.com>
2020-01-14 17:16:07 +0100

    ojoooooooooooooooooooo

---
+++ Ojo.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+/**
+ * Write a description of class Potato here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Ojo
+{
+  public static void main (String[]args)
+  {
+   boolean ojo=true;
+   while(ojo)
+   {
+    Scanner sc=new Scanner(System.in);
+    String s=sc.nextLine();
+    if(s.equals(ojo))
+    {
+       System.out.println("ojoooooooooo");
+       ojo=false;
+    }
+    System.out.println("indique la palabra correcta");
+    
+   }
+   
+    }
+}
