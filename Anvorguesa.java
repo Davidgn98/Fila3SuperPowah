d26be6efb0609be91f8c4997cad41da2ec2b0efe
mimi <rachelortiz1999@gmail.com>
2020-01-17 18:23:48 +0100

    anvorguesa documentada

---
+++ Anvorguesa.java
@@ -0,0 +1,30 @@
+
+/**
+ * Write a description of class ciruela here.
+ * 
+ * @author (Raquel) 
+ * @version (17.01.2020 1.0v)
+ */
+public class Anvorguesa
+{
+    private int x;
+
+    /**
+     * Constructores de la clase Anvorguesa
+     */
+    public Anvorguesa()
+    {
+        x = 0;
+    }
+
+    /**
+     * Este metodo hará que el michi pida 10 anvorguesas
+     */
+    public void anvorguesaParaElMichi()
+    {
+        for(int i=0;i<11;i++)
+        {
+          System.out.println("el michi quiere anvorguesa");
+        }
+    }
+}
